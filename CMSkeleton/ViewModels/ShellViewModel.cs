1d6d6f30270fb6b8c259fdb8da7099677015965a
rohits79 <rohits79@gmail.com>
2016-05-27 10:18:22 +0800

    Basic skeleton

---
+++ CMSkeleton/ViewModels/ShellViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Caliburn.Micro;
+using Action = System.Action;
+
+namespace AutoDumper.ViewModels
+{
+    public class ShellViewModel : PropertyChangedBase
+    { 
+        public ShellViewModel()
+        {
+           //Init
+        }
+    }
+
+}
